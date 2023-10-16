d16d38e4c0dc683af9279d30b6b454df35c3522f
PANxxJ <ps789875@gmail.com>
2023-10-16 13:26:30 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import store from "./store";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://127.0.0.1:8000";
-axios.defaults.baseURL = "http://127.0.0.1:9000";
+axios.defaults.baseURL = "https://invoicely.onrender.com";
 
 createApp(App).use(store).use(router, axios).mount("#app");
